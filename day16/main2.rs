d089a068bbc1b107a846c89681b406978c787a48
Alec Snyder <linuxbash8@gmail.com>
2018-11-09 09:56:42 -0800

    finish day16 part 2

--- day16/main2.rs
+++ day16/main2.rs
@@ -1,6 +1,68 @@
 mod util;
 
+use std::collections::HashSet;
+
 const ARR_SIZE: usize = 16;
+
+struct Dance {
+    index_arr: [u8; ARR_SIZE],
+    char_arr: [char; ARR_SIZE],
+}
+
+impl Dance {
+    fn new() -> Dance {
+        Dance {
+            index_arr: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+            char_arr: [
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            ]
+        }
+    }
+
+    fn rotate(&mut self, num: u8) {
+        for i in 0..ARR_SIZE {
+            self.index_arr[i] = (self.index_arr[i] + num) % ARR_SIZE as u8;
+            self.char_arr[self.index_arr[i] as usize] = (i as u8 + 97) as char;
+        }
+    }
+
+    fn swap_index(&mut self, from_index: usize, to_index: usize) {
+        let temp = self.char_arr[to_index];
+        self.char_arr[to_index] = self.char_arr[from_index];
+        self.index_arr[self.char_arr[to_index] as usize - 97] = to_index as u8;
+
+        self.char_arr[from_index] = temp;
+        self.index_arr[temp as usize - 97] = from_index as u8;
+    }
+
+    fn swap_char(&mut self, from_char: char, to_char: char) {
+        let from_char_idx: usize = from_char as usize - 97;
+        let to_char_idx: usize = to_char as usize - 97;
+
+        let temp = self.index_arr[to_char_idx];
+        self.index_arr[to_char_idx] = self.index_arr[from_char_idx];
+        self.char_arr[self.index_arr[to_char_idx] as usize] = to_char;
+
+        self.index_arr[from_char_idx] = temp;
+        self.char_arr[self.index_arr[from_char_idx] as usize] = from_char;
+    }
+
+    fn print_array(&self) {
+        print!("array is: ");
+        for i in 0..ARR_SIZE {
+            print!("{}", self.char_arr[i]);
+        }
+        println!("");
+    }
+
+    fn get_arr_str(&self) -> String {
+        let mut arr_str: String = String::new();
+        for i in 0..ARR_SIZE {
+            arr_str += &format!("{}", self.char_arr[i]);
+        }
+        return arr_str;
+    }
+}
 
 enum Instruction {
     Spin(usize),
@@ -9,54 +71,43 @@ enum Instruction {
 }
 
 fn main() {
+    let total_num_iters = 1000000000;
     let instructions = get_instructions();
-    let mut arr = get_start_arr();
-    count_partner(&instructions);
+    let mut dance = Dance::new();
+    let mut seen_before: HashSet<String> = HashSet::new();
+    let mut num_iters: Option<i32> = Option::None;
 
-    for i in 0..1000000000 {
-        run_iteration(&mut arr);
-    }
-
-    print!("Array is: ");
-    for i in 0..ARR_SIZE {
-        print!("{}", arr[i]);
-    }
-    println!("");
-}
-
-fn get_start_arr() -> [char; ARR_SIZE] {
-    [
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-    ]
-}
-
-fn rotate(num: usize, arr: [char; ARR_SIZE]) -> [char; ARR_SIZE] {
-    let mut new_arr = ['\0'; ARR_SIZE];
-    for i in 0..ARR_SIZE {
-        let new_index = (i + num) % ARR_SIZE;
-        new_arr[new_index] = arr[i];
-    }
-    return new_arr;
-}
-
-fn swap_index(arr: &mut [char; ARR_SIZE], from_index: usize, to_index: usize) {
-    let temp = arr[to_index];
-    arr[to_index] = arr[from_index];
-    arr[from_index] = temp;
-}
-
-fn swap_chars(arr: &mut [char; ARR_SIZE], from_char: char, to_char: char) {
-    let mut from_index = ARR_SIZE;
-    let mut to_index = ARR_SIZE;
-
-    for i in 0..ARR_SIZE {
-        if arr[i] == from_char {
-            from_index = i;
-        } else if arr[i] == to_char {
-            to_index = i;
+    for i in 0..total_num_iters {
+        for instruction in &instructions {
+            match instruction {
+                Instruction::Spin(s) => dance.rotate(*s as u8),
+                Instruction::Exchange(x, y) => dance.swap_index(*x, *y),
+                Instruction::Partner(a, b) => dance.swap_char(*a, *b),
+            }
+        }
+        let this_dance = dance.get_arr_str();
+        if seen_before.contains(&this_dance) {
+            num_iters = Some(total_num_iters % i);
+            break;
+        } else {
+            seen_before.insert(this_dance);
         }
     }
-    swap_index(arr, from_index, to_index);
+
+    println!("num_cycles is: {}", num_iters.unwrap());
+
+    dance = Dance::new();
+
+    for _ in 0..num_iters.unwrap() {
+        for instruction in &instructions {
+            match instruction {
+                Instruction::Spin(s) => dance.rotate(*s as u8),
+                Instruction::Exchange(x, y) => dance.swap_index(*x, *y),
+                Instruction::Partner(a, b) => dance.swap_char(*a, *b),
+            }
+        }
+    }
+    dance.print_array();
 }
 
 fn get_instructions() -> Vec<Instruction> {
@@ -86,20 +137,6 @@ fn get_instructions() -> Vec<Instruction> {
     return instructions;
 }
 
-fn count_partner(instructions: &Vec<Instruction>) {
-    let total = instructions.len();
-    let mut num_partner = 0;
-
-    for instruction in instructions {
-        match instruction {
-            Instruction::Partner(_, _) => num_partner += 1,
-            _ => { }
-        }
-    }
-
-    println!("partnering is {} out of {}", num_partner, total);
-}
-
 fn split_input(input: &[u8]) -> (&[u8], &[u8]) {
     let mut i = 0;
     while i < input.len() {
@@ -109,27 +146,4 @@ fn split_input(input: &[u8]) -> (&[u8], &[u8]) {
         i += 1;
     }
     return (&input[0..i], &input[i + 1..input.len()]);
-}
-
-fn run_iteration(arr: &mut [char; ARR_SIZE]) {
-    let arr_clone = arr.clone();
-    if *arr == arr_clone {
-
-    }
-    arr[0] = arr_clone[8];
-    arr[1] = arr_clone[14];
-    arr[2] = arr_clone[13];
-    arr[3] = arr_clone[11];
-    arr[4] = arr_clone[1];
-    arr[5] = arr_clone[10];
-    arr[6] = arr_clone[5];
-    arr[7] = arr_clone[4];
-    arr[8] = arr_clone[0];
-    arr[9] = arr_clone[9];
-    arr[10] = arr_clone[6];
-    arr[11] = arr_clone[3];
-    arr[12] = arr_clone[12];
-    arr[13] = arr_clone[15];
-    arr[14] = arr_clone[7];
-    arr[15] = arr_clone[2];
 }
